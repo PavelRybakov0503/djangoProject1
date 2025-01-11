4929bb76b67af9456d6396201060091d5826481e
pavel rybakov <pavel.r.2015@mail.ru>
2025-01-11 15:39:03 +0500

    urls

---
+++ catalog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.utils.translation.trans_real import catalog
+
+from catalog.apps import CatalogConfig
+from catalog.views import home_view, contacts_view
+
+app_name = CatalogConfig.name
+
+urlpatterns = [
+    path('home/', home_view, name='home'),
+    path('contact/', contacts_view, name='contact'),
+]

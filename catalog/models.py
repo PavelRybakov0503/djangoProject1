26566ea4241f3d48bd5814124c65035b74434841
pavel rybakov <pavel.r.2015@mail.ru>
2025-01-28 17:15:23 +0500

    Домашнее задание 24. В models.py (description) длина 200. В products_detail.html добавлено |truncatechars:100 }} — сокращает текст переменной .description  до 100 символов.

--- catalog/models.py
+++ catalog/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(
-        max_length=100,
+        max_length=150,
         verbose_name='Наименование',
         help_text='Введите наименование'
     )   # наименование,
     description = models.CharField(
-        max_length=100,
+        max_length=200,
         verbose_name='Описание',
         help_text='Введите описание'
     )  # описание,
